ac44319771745b0e53c9c91e095f0be69f569dc6
Connie <leungcnie@gmail.com>
2020-11-26 22:27:45 +0000

    Delete dead code from addProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -145,10 +145,6 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  // const propertyId = Object.keys(properties).length + 1;
-  // property.id = propertyId;
-  // properties[propertyId] = property;
-  // return Promise.resolve(property);
   const queryParams = [
     property.owner_id, 
     property.title, 
